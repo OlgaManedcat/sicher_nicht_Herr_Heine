6ec75025f09004ab6020cc73922c7de97907af38
Icaruslikesfalling <geisthardt.noah@gmail.com>
2026-02-12 11:54:40 +0100

    added class

--- Oraculbvisual/MainWindow.xaml.cs
+++ Oraculbvisual/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Oraculbvisual
 {
@@ -24,18 +13,19 @@ namespace Oraculbvisual
         {
             InitializeComponent();
             MessageBox.Show("In einem weit entverntem Land in Babendorf, in den Tiefen, des Schulgebaude-Dschungles " +
-                "im Raum A1.25 in einem Zelt ist jemand (der sicherlich nicht Sebastian Heine heißt) zu finden. " +
-                "Er hat großes Wissen uber zukunft");
-            MessageBox.Show("Das Orakel: Hallo Wanderer, meine name ist Herr Hei....*Pause* unwichtig eigentlich. " +
+                "im Raum A1.25 in einem Zelt ist Jemand (der sicherlich nicht Sebastian Heine heißt) zu finden. " +
+                "Er hat großes Wissen über die Zukunft");
+            MessageBox.Show("Das Orakel: Hallo Wanderer, meine Name ist Herr Hei....*Pause* unwichtig eigentlich. " +
                 "Ich weiß viel und wenn du Fragen über die Zukunft hast stell sie mir, vieliecht werde ich sie antworten.");
         }
-       
+
         private void TextBox_TBAusgabe() { }
 
-      
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            return;
+            MessageBox.Show("Tschüssiiiii!!!");
+            this.Close();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
---
+++ Oraculbvisual/Orakel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Oraculbvisual
+{
+    internal class Orakel
+    {
+        public int Zahl;
+        public Orakel()
+        {
+            Zahl = 0;
+        }
+        public Orakel(int Zahl, string TbEingabe)
+        {
+            while (Zahl < 10)
+            { 
+              if (TbEingabe.Contains("+","-","*","/",""))
+                {
+                    MessageBox.Show("Das kann man doch im Kopf rechnen!");
+                }
+            }
+        }
+    }
+}
