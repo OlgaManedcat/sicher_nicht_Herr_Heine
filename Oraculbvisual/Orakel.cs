5ceefc0f39146199a017096742d192198b1632e1
OlgaManedcat <98811012+OlgaManedcat@users.noreply.github.com>
2026-03-02 09:35:16 +0100

    further merge

--- Oraculbvisual/Orakel.cs
+++ Oraculbvisual/Orakel.cs
@@ -18,40 +18,28 @@ namespace Oraculbvisual
         }
         public Orakel(int Zahl, string TbEingabe)
         {
-            while (Zahl < 10)
-            {
-                if (TbEingabe.Contains("+") || TbEingabe.Contains("-") || TbEingabe.Contains("*") || TbEingabe.Contains("/") || TbEingabe.Contains("π"))
+                while (Zahl < 3)
                 {
-                    MessageBox.Show("Das kann man doch im Kopf rechnen!");
+                    if (TbEingabe.Contains("+") || TbEingabe.Contains("-") || TbEingabe.Contains("*") || TbEingabe.Contains("/") || TbEingabe.Contains("π"))
+                    {
+                        MessageBox.Show("Das kann man doch im Kopf rechnen!");
+                        return;
+                    }
+                    if (TbEingabe.Contains("seh") || TbEingabe.Contains("sieh"))
+                    {
+                        MessageBox.Show("jung, dynamisch und skelletmäßig!");
+                        return;
+                    }
+                    if (TbEingabe.Contains("Wetter"))
+                    {
+                        MessageBox.Show("Guck aus dem Fenster!");
+                        return;
+                    }
+                    Zahl++;
                 }
-                if (TbEingabe.Contains("seh") || TbEingabe.Contains("sieh"))
-                {
-                    MessageBox.Show("jung, dynamisch und skelletmäßig!");
-                }
-                if (TbEingabe.Contains("Wetter"))
-                {
-                    MessageBox.Show("Guck aus dem Fenster!");
-                }
-                else
-                {
-                    Random rnd = new Random();
-                    int mvorAntwort = rnd.Next(1, 6);
-                    if (mvorAntwort = 1 )
-                       { mAntwort = "ja"; }
-                    if (mvorAntwort = 2 )
-                    { mvorAntwort = "nein"; }
-                    if (mvorAntwort = 3)
-                    {mvorAntwort = "veilicht" }
-                    if (mvorAntwort = 4 )
-                    { mvorAntwort = "frag mich Morgen"}
-                    if (mvorAntwort = 5 )
-                    
-                    MessageBox.Show($"{mAntwort}");
-                    
-                }
-
-    }
-        Zahl++;
-            }
+            MessageBox.Show("Ach, lass mich doch in Ruhe!");
+            Environment.Exit(100);
         }
     }
+}
+
