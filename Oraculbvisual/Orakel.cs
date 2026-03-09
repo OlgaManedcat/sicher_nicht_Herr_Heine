ecac0f1fa0e1d96742ab2d313fdb36cd8724a258
Icaruslikesfalling <geisthardt.noah@gmail.com>
2026-03-09 09:28:30 +0100

    Zufallsmethode begonnen

--- Oraculbvisual/Orakel.cs
+++ Oraculbvisual/Orakel.cs
@@ -11,34 +11,48 @@ namespace Oraculbvisual
 {
     internal class Orakel
     {
-        public int Zahl;
-        public Orakel()
-        {
-            Zahl = 0;
-        }
+        public int Zahl = 0;
+  
         public Orakel(int Zahl, string TbEingabe)
         {
-                while (Zahl < 3)
+            while (Zahl < 3)
+            {
+                Zahl++;
+                if (TbEingabe.Contains("+") || TbEingabe.Contains("-") || TbEingabe.Contains("*") || TbEingabe.Contains("/") || TbEingabe.Contains("π") || TbEingabe.Contains("pi"))
                 {
-                    if (TbEingabe.Contains("+") || TbEingabe.Contains("-") || TbEingabe.Contains("*") || TbEingabe.Contains("/") || TbEingabe.Contains("π"))
-                    {
-                        MessageBox.Show("Das kann man doch im Kopf rechnen!");
-                        return;
-                    }
-                    if (TbEingabe.Contains("seh") || TbEingabe.Contains("sieh"))
-                    {
-                        MessageBox.Show("jung, dynamisch und skelletmäßig!");
-                        return;
-                    }
-                    if (TbEingabe.Contains("Wetter"))
-                    {
-                        MessageBox.Show("Guck aus dem Fenster!");
-                        return;
-                    }
-                    Zahl++;
+                    MessageBox.Show("Das kann man doch im Kopf rechnen!");
+                    return;
                 }
-            MessageBox.Show("Ach, lass mich doch in Ruhe!");
-            Environment.Exit(100);
+                else if (TbEingabe.Contains("seh") || TbEingabe.Contains("sieh"))
+                {
+                    MessageBox.Show("jung, dynamisch und skelletmäßig!");
+                    return;
+                }
+                else if (TbEingabe.Contains("Wetter"))
+                {
+                    MessageBox.Show("Guck aus dem Fenster!");
+                    return;
+                }
+                else
+                {
+                    Zufallsmethode();
+                    return;
+                }        
+            }
+            if (Zahl == 3)
+            {
+
+                MessageBox.Show("Ach, lass mich doch in Ruhe!");
+                Environment.Exit(100);
+            }
+        }
+        void Zufallsmethode()
+        {
+            string[] zufallsAusgaben = { "Ja", "Nein", "Wurzel aus 7", "Vielleicht", "Da solltest du nochmal drüber nachdenken", "Sicher", "Das Probelem befindet sich meist 1/2 Meter vor dem Bildschirm" };
+            Random rd = new Random();
+            int zahl = rd.Next(0, 6);
+            MessageBox.Show(zufallsAusgaben[zahl]);
+            return;
         }
     }
 }
